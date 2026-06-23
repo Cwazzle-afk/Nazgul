763ee04f9159e58ae4e4a9e5948b6a04e0c649db
Cade's laptop <221428216+Cwazzle-afk@users.noreply.github.com>
2026-06-22 21:12:01 -0400

    added math for roaming throne

--- nazgul_counters.py
+++ nazgul_counters.py
@@ -1,36 +1,56 @@
+import os
 number_of_nazgul = 0
 nazgul_on_battlefield = []
-import os
+
 
 def clear_terminal():
     os.system("cls" if os.name == "nt" else "clear")
 
 def ask_for_input():
-    #clear_terminal()
+    clear_terminal()
     display_nazgul()
     user_input = int(input("(1) Add a nazgul to the battlefield\n"
                    "(2) Tempt the ring\n"
                    "(3) Remove a nazgul\n"
                    "(0) Exit"
-                   "\nPlease enter what you would like to do: "
+                   "\n\nPlease enter what you would like to do: "
                    ))
     return user_input
 
 def tempt_the_ring():
     
+    roaming_throne_on_battlefield = False
+    
+    for name in nazgul_on_battlefield:
+        if name["name"] == 'roaming throne':
+            roaming_throne_on_battlefield = True
+
     for counter in nazgul_on_battlefield:
-        counter["counters"] += int(len(nazgul_on_battlefield))
+        if not roaming_throne_on_battlefield:
+            counter["counters"] += int(len(nazgul_on_battlefield))
+        elif roaming_throne_on_battlefield:
+            counter["counters"] += int((len(nazgul_on_battlefield) - 1) * 2)
+ 
 
 def display_nazgul():
-    name = ''
+    names = []
     counters = 0
-
+    
+    if len(nazgul_on_battlefield) == 0:
+        print("\nNo Nazgul on battlefield yet\n")
+        return
+    for name in nazgul_on_battlefield:
+        names.append(name["name"])
+    print("Nazgul on the battlefield: ", end='')
+    for nazgul in names:
+        print(nazgul, end=', ')
+    print("\n\n")
     for display in nazgul_on_battlefield:
-        print(f"Nazgul: {display["name"]} has {display["counters"]} counters.")
-           
+        print(f"Counters on Nazgul: {display["name"]} has {display["counters"]} counters.\n")
 
 def add_nazgul():
-    print(f"Nazgul already on the battlefield: {nazgul_on_battlefield}") 
+    clear_terminal()
+    display_nazgul() 
     nazgul_name = ''
     counters = 0
     
@@ -60,10 +80,8 @@ def add_nazgul():
     nazgul_on_battlefield.append(nazgul_to_add)
 
     print(f"\nnumber of Nazgul: {len(nazgul_on_battlefield)}\n")
-    print(f"Nazgul on battlefield: {nazgul_on_battlefield}")
     
-
-    
+        
 user_input = ask_for_input()
 
 while user_input != 0:
